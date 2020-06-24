cd117a037f06f0f60c1c9fefe362687ebe5be3c7
SofianeBlr <sofiane0297@gmail.com>
2020-06-24 14:33:46 +0200

    verification date

--- src/main/java/com/excilys/computerDatabase/model/Computer.java
+++ src/main/java/com/excilys/computerDatabase/model/Computer.java
@@ -23,14 +23,31 @@ public class Computer {
 	public LocalDate getIntroduced() {
 		return introduced;
 	}
-	public void setIntroduced(LocalDate introduced) {
-		this.introduced = introduced;
+	public void setIntroduced(LocalDate introduced) throws IllegalArgumentException{
+		if(introduced == null || this.discontinued == null) {
+			this.introduced = introduced;
+		}
+		else if(this.introduced.isBefore(discontinued)) {
+			this.introduced = introduced;
+		}
+		else {
+			throw new IllegalArgumentException("discontinued date < introduced date");
+		}
 	}
 	public LocalDate getDiscontinued() {
 		return discontinued;
 	}
-	public void setDiscontinued(LocalDate discontinued) {
-		this.discontinued = discontinued;
+	public void setDiscontinued(LocalDate discontinued) throws IllegalArgumentException {
+		if(this.introduced == null || discontinued==null) {
+			this.discontinued = discontinued;
+		}
+		else if(this.introduced.isBefore(discontinued)) {
+			this.discontinued = discontinued;
+		}
+		else {
+			throw new IllegalArgumentException("discontinued date < introduced date");
+		}
+
 	}
 	public int getCompanyId() {
 		return companyId;
@@ -38,12 +55,12 @@ public class Computer {
 	public void setCompanyId(int companyId) {
 		this.companyId = companyId;
 	}
-	public Computer(int id, String name, LocalDate introduced, LocalDate discontinued, int companyId) {
+	public Computer(int id, String name, LocalDate introduced, LocalDate discontinued, int companyId) throws IllegalArgumentException {
 		this.id = id;
 		this.name = name;
-		this.introduced = introduced;
-		this.discontinued = discontinued;
 		this.companyId = companyId;
+		setIntroduced(introduced);
+		setDiscontinued(discontinued);
 	}
 	public Computer(int id, String name) {
 		super();
@@ -64,19 +81,21 @@ public class Computer {
 		this.introduced = introduced;
 	}
 	public Computer(int id, String name, int companyId) {
+		super();
 		this.id = id;
 		this.name = name;
 		this.companyId = companyId;
 	}
 	public Computer(int id) {
+		super();
 		this.id = id;
 	}
-	public Computer(int id, String name, LocalDate introduced, LocalDate discontinued) {
+	public Computer(int id, String name, LocalDate introduced, LocalDate discontinued)throws IllegalArgumentException {
 		super();
 		this.id = id;
 		this.name = name;
-		this.introduced = introduced;
-		this.discontinued = discontinued;
+		setIntroduced(introduced);
+		setDiscontinued(discontinued);
 	}
 	public Computer() {
 	}
@@ -87,42 +106,42 @@ public class Computer {
 		return String.format("  %d  %40s  %20s%20s%5d%n",getId(),getName(),i,d,getCompanyId());
 	}
 	@Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        Computer other = (Computer) obj;
-        if (companyId!=other.companyId) {
-            return false;
-        }
-        if (discontinued == null) {
-            if (other.discontinued != null) {
-                return false;
-            }
-        } else if (!discontinued.equals(other.discontinued)) {
-            return false;
-        }
-        if (introduced == null) {
-            if (other.introduced != null) {
-                return false;
-            }
-        } else if (!introduced.equals(other.introduced)) {
-            return false;
-        }
-        if (name == null) {
-            if (other.name != null) {
-                return false;
-            }
-        } else if (!name.equals(other.name)) {
-            return false;
-        }
-        return true;
-    }
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Computer other = (Computer) obj;
+		if (companyId!=other.companyId) {
+			return false;
+		}
+		if (discontinued == null) {
+			if (other.discontinued != null) {
+				return false;
+			}
+		} else if (!discontinued.equals(other.discontinued)) {
+			return false;
+		}
+		if (introduced == null) {
+			if (other.introduced != null) {
+				return false;
+			}
+		} else if (!introduced.equals(other.introduced)) {
+			return false;
+		}
+		if (name == null) {
+			if (other.name != null) {
+				return false;
+			}
+		} else if (!name.equals(other.name)) {
+			return false;
+		}
+		return true;
+	}
 
 }
