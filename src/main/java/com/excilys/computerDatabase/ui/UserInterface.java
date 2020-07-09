55ee7b5ae675e7d6757c7fe78956e4c0b8ed32f0
SofianeBlr <sofiane0297@gmail.com>
2020-07-09 10:02:10 +0200

    ui

--- src/main/java/com/excilys/computerDatabase/ui/UserInterface.java
+++ src/main/java/com/excilys/computerDatabase/ui/UserInterface.java
@@ -6,7 +6,6 @@ import java.time.format.DateTimeParseException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
