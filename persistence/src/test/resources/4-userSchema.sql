dc788ad919553be5d722ba64afb0a3da32c50965
SofianeBlr <sofiane0297@gmail.com>
2020-08-07 15:25:13 +0200

    new users

--- persistence/src/test/resources/4-userSchema.sql
+++ persistence/src/test/resources/4-userSchema.sql
@@ -7,8 +7,11 @@ create table role (
     role_name                      varchar(255),
     constraint pk_user primary key (id))
   ;
-  insert into role(role_name) VALUES('user');
+  
+   insert into role(role_name) VALUES('superadmin');
   insert into role(role_name) VALUES('admin');
+  insert into role(role_name) VALUES('user');
+    insert into role(role_name) VALUES('anon');
 
   create table user (
     id                        bigint not null auto_increment,
@@ -18,5 +21,7 @@ create table role (
     constraint pk_user primary key (id))
   ;
    alter table user add constraint fk_user_role_1 foreign key (role_id) references role (id) on delete restrict on update restrict;
-  insert into user(username,password,role_id) VALUES('user','$2a$10$EblZqNptyYvcLm/VwDCVAuBjzZOI7khzdyGPBr08PpIi0na624b8.',1);
+  insert into user(username,password,role_id) VALUES('superadmin','$2a$10$EblZqNptyYvcLm/VwDCVAuBjzZOI7khzdyGPBr08PpIi0na624b8.',1);
   insert into user(username,password,role_id) VALUES('admin','$2a$10$EblZqNptyYvcLm/VwDCVAuBjzZOI7khzdyGPBr08PpIi0na624b8.',2);
+  insert into user(username,password,role_id) VALUES('user','$2a$10$EblZqNptyYvcLm/VwDCVAuBjzZOI7khzdyGPBr08PpIi0na624b8.',3);
+  insert into user(username,password,role_id) VALUES('anon','$2a$10$EblZqNptyYvcLm/VwDCVAuBjzZOI7khzdyGPBr08PpIi0na624b8.',4);
